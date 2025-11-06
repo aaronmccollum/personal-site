c473001e9696c0feb66ce9a7af0cc89c1d33d1df
aaronmccollum <mccollumaarons@gmail.com>
2025-11-06 09:35:37 +0900

    commiting everything this time

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+const resumeBtn = document.getElementById("resume-btn");
+
+const downloadFile = () => {
+    const url = "files/AaronMcCollum_Resume.pdf";
+
+    let req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.responseType = "blob";
+    req.onload = function() {
+        let blob = new Blob([req.response], { type: "application/octetstream" });
+        let url = window.URL ||window.webkitURL;
+        link = url.createObjectURL(blob);
+        let a = document.createElement("a");
+        a.setAttribute("download", fileName);
+        a.setAttribute("href", link);
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    }
+    req.send();
+    console.log("Hello!");
+}
+
+resumeBtn.addEventListener('click', downloadFile);
